6173204350437ce62eb02849fde1a8c343dc5c97
Tristan Zinn <tristanzinn@MacBook-Pro-von-Tristan.local>
2024-12-08 17:51:50 +0100

    Your commit message

--- src/data/folderStructure.ts
+++ src/data/folderStructure.ts
@@ -33,6 +33,12 @@ export const folderStructure: MainFolder[] = [
             url: "https://github.com/zinnt0/Schuelermaterialien/blob/b3c5b9793176a0b9aba6609d65d6df117fec26ca/WP%20Informatik/Spielvorlage.py",
             target: "_blank"
             
+          },
+          {
+            name: "Szenarien",
+            url: "https://github.com/zinnt0/Schuelermaterialien/blob/ba106a47183f2c39524234cf84747b50ebe16e95/WP%20Informatik/Szenarien.md",
+            target: "_blank"
+            
           }
         ]
       }
